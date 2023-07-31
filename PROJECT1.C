e48f881f9313dd128b11bcda7a20cc5f515b691c
dharmik <dharmikkhunt75@gmail.com>
2023-07-31 19:12:32 +0530

    main

---
+++ PROJECT1.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	long int tp=80000;
+	double nom,now,lm,lw,tl,ilm,ilw;
+	clrscr();
+
+	printf("1. Total Population is: %ld",tp);
+
+	nom = tp * 0.52;
+	printf("\n2. Num of men is: %.0f",nom);
+
+	now = tp * 0.48;
+	printf("\n3. Num of women is: %.0f",now);
+
+	lm = tp * 0.35;
+	printf("\n4. Num of literate men is: %.0f",lm);
+
+	lw = tp * 0.13;
+	printf("\n5. Num of literate women is: %.0f",lw);
+
+	tl = lm + lw;
+	printf("\n6. Total literate: %.0f",tl);
+
+	ilm = nom - lm;
+	printf("\n7. Illetrate men is: %.0f",ilm);
+
+	ilw = now - lw;
+	printf("\n8. Illetrate women is: %.0f",ilw);
+
+	getch();
+	return 0;
+}
